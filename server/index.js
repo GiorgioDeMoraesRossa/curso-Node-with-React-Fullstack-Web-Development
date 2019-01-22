db03d9a0e420441c406d22d9d0e41f6474f28b36
Giorgio_Rossa <giorgiorossa@inf.ufpel.edu.br>
2019-01-22 18:32:48 -0200

    att flow

--- server/index.js
+++ server/index.js
@@ -1,9 +1,26 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
+const cookiSession = require("cookie-session");
+const passport = require("passport");
+require("./models/user");
+require("./services/passport");
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
+
+app.use(
+  cookiSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookiSession]
+  })
+);
+
+app.use(passport.initialize);
+app.use(passport.session());
+
+require("./routes/authRoutes")(app);
+
 const PORT = process.env.PORT || 5000;
-
-app.get("/", (req, res) => {
-  res.send({ hi: "there" });
-});
-
 app.listen(PORT);
