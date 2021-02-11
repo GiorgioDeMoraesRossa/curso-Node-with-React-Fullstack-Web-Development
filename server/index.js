21862097bfc4e9eb3a025033ddb268b95bcf7ec6
Giorgio Rossa <giorgiorossa@inf.ufpel.edu.br>
2021-02-11 01:21:09 -0200

    client e payment

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const keys = require("./config/keys");
 const cookiSession = require("cookie-session");
 const passport = require("passport");
+const bodyParser = require("body-parser");
 require("./models/user");
 require("./services/passport");
 
@@ -10,10 +11,11 @@ mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(
   cookiSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
-    keys: [keys.cookieKey]
+    keys: [keys.cookieKey],
   })
 );
 
@@ -21,6 +23,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require("./routes/authRoutes")(app);
+require("./routes/billingRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
